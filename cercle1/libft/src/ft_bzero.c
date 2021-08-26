5853f4ae5c358feaf295c35dc3ca3b93702e9fa7
maxime <maxime.cauchy93@gmail.com>
2021-08-26 16:23:41 +0200

    push auto

--- cercle1/libft/src/ft_bzero.c
+++ cercle1/libft/src/ft_bzero.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maximecauchy <maximecauchy@student.42.f    +#+  +:+       +#+        */
+/*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/05 12:05:30 by maximecauch       #+#    #+#             */
-/*   Updated: 2021/08/05 13:04:05 by maximecauch      ###   ########.fr       */
+/*   Created: 2021/08/05 12:05:30 by maximecauchy      #+#    #+#             */
+/*   Updated: 2021/08/05 13:04:05 by maximecauchy     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
