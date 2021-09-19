7a506fce1466853f7dd7e533a4aa57ec64459cb6
maxime <maxime.cauchy93@gmail.com>
2021-09-19 22:45:29 +0200

    push auto

--- cercle1/libft/src/ft_itoa.c
+++ cercle1/libft/src/ft_itoa.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   itoa.c                                             :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maximecauchy <maximecauchy@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 09:01:39 by maximecauch       #+#    #+#             */
-/*   Updated: 2021/09/17 13:42:48 by maximecauch      ###   ########.fr       */
+/*   Updated: 2021/09/18 13:27:12 by maximecauch      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+#include "limits.h"
 
 int	ft_len(int nb)
 {
 	int	size;
 
 	size = 0;
+	if (nb == -2147483648)
+		return (11);
+	if (nb == 0)
+		return (1);
 	if (nb < 0)
 	{
-		nb = nb * -1;
+		nb *= -1;
 		size++;
 	}
 	while (nb > 0)
@@ -30,19 +35,26 @@ int	ft_len(int nb)
 	return (size);
 }
 
+char	*ft_intmin(char *str)
+{
+	str = "-2147483648\0";
+	return (str);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
 	int		len;
 
 	len = ft_len(n);
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
+	if (!(str = (char *)malloc(sizeof(char) * (ft_len(n) + 1))))
 		return (NULL);
-	str[n - 1] = '\0';
+	str[len] = '\0';
+	if (n == -2147483648)
+		return (ft_intmin(str));
 	if (n == 0)
 	{
-		str[0] = '0';
+		str = "0\0";
 		return (str);
 	}
 	if (n < 0)
@@ -58,3 +70,11 @@ char	*ft_itoa(int n)
 	}
 	return (str);
 }
+
+/*
+int	main()
+{
+	printf("%s\n", ft_itoa(INT_MIN));
+	return 0;
+}
+*/
