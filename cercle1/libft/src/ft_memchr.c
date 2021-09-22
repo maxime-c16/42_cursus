1b72df7d006794c51b90c96cf0594a47a5f13f54
maxime <maxime.cauchy93@gmail.com>
2021-09-22 17:33:03 +0200

    push auto

--- cercle1/libft/src/ft_memchr.c
+++ cercle1/libft/src/ft_memchr.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maximecauchy <maximecauchy@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 17:31:57 by maximecauch       #+#    #+#             */
+/*   Updated: 2021/09/22 17:31:58 by maximecauch      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft.h"
 
 void	*ft_memchr(const void *s, int c, int n)
 {
-	unsigned char	*ptr;	
+	unsigned char	*ptr;
 	int				i;
 	unsigned char	a;
 
