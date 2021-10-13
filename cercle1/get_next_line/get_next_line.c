4fa3b4fbe6d336ab9f8944175443f8560da42d9b
maxime <maxime.cauchy93@gmail.com>
2021-10-13 19:48:33 +0200

    push auto

--- cercle1/get_next_line/get_next_line.c
+++ cercle1/get_next_line/get_next_line.c
@@ -1,6 +1,51 @@
 #include "libft/libft.h"
 #include "get_next_line.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s3)
+		return (NULL);
+	s3 = strcat(s3, (char *)s1);
+	s3 = strcat(s3, (char *)s2);
+	return (s3);
+}
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+	int		i;
+
+	i = 0;
+	ptr = (char *)s;
+	while (ptr[i])
+	{
+		if (ptr[i] == (char)c)
+			return (ptr + i);
+		i++;
+	}
+	if ((char)c == '\0')
+		return (ptr + i);
+	else
+		return (NULL);
+}
+
 char	*ft_gauche(char *str)
 {
 	int		i;
@@ -20,7 +65,6 @@ char	*ft_gauche(char *str)
 	}
 	dest[i] = '\0';
 	return (dest);
-
 }
 
 char	*ft_droite(char *str)
@@ -42,7 +86,7 @@ char	*ft_droite(char *str)
 	return (dest);
 }
 
-int	get_next_line(int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
 	static char	*str = NULL;
 	char		*temp;
@@ -71,18 +115,19 @@ int	get_next_line(int fd, char **line)
 	return (ret);
 }
 
-int	main()
+int	main(void)
 {
 	int		fd;
 	int		ret;
 	char	*line;
+	int i = 0;
 
 	fd = open("alternate_line_nl_with_nl", 0);
 	ret = get_next_line(fd, &line);
-	while (ret != 0)
+	while (i++ < 1)
 	{
-		printf("%s\n", line);
 		ret = get_next_line(fd, &line);
+		printf("%s\n", line);
 	}
 	return (0);
 }
